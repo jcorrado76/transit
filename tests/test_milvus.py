36a76da264b3a218faa861fb08a7a6c0ad73833d
Joseph Corrado <josephcorrado7@gmail.com>
2023-04-22 17:00:11 -0400

    add context manager for milvus repository

--- tests/test_milvus.py
+++ tests/test_milvus.py
@@ -8,7 +8,7 @@ from transit import milvus
 @pytest.fixture(scope='module')
 def local_milvus_connections():
     connections = Connections()
-    milvus.connect_to_milvus_server(alias='default', host='localhost', port='19530')
+    milvus.MilvusRepository.connect_to_milvus_server(alias='default', host='localhost', port='19530')
     return connections
 
 
@@ -26,11 +26,43 @@ def test_can_connect_to_local_milvus_server(local_milvus_connections):
     host='localhost'
     port='19530'
     alias='default'
-    milvus.connect_to_milvus_server(host, port, alias)
+    milvus.MilvusRepository.connect_to_milvus_server(host, port, alias)
     assert local_milvus_connections.has_connection(alias=alias)
 
 
 def test_can_create_collection(local_milvus_connections):
     alias_name = 'default'
-    milvus.create_collection(alias_name)
+    milvus.MilvusRepository.create_collection(alias_name)
     assert local_milvus_connections.has_connection(alias=alias_name)
+
+def test_can_instantiate_fake_milvus_connection():
+    connection = milvus.FakeMilvusConnection()
+
+def test_can_connect_and_disconnect_fake_milvus_collection():
+    connection = milvus.FakeMilvusConnection()
+    connection.connect()
+
+    connection.disconnect()
+
+def test_can_get_connections(local_milvus_connections):
+    connections = milvus.MilvusRepository.get_connections()
+    assert connections.has_connection('default')
+
+def test_can_assert_connection_exists(local_milvus_connections):
+    milvus.MilvusRepository.assert_connection_exists('default')
+
+def test_can_instantiate_local_fake_milvus_connection():
+    connection = milvus.FakeMilvusConnection.local_connection(
+        'default',
+        host='localhost',
+        port='19530'
+    )
+    assert connection is not None
+
+
+def test_can_instantiate_milvus_repository():
+    milvus_repository = milvus.MilvusRepository()
+
+def test_can_instantiate_milvus_with_context_manager():
+    with milvus.MilvusRepository() as repository:
+        assert repository is not None
--- transit/milvus.py
+++ transit/milvus.py
@@ -1,11 +1,18 @@
 """
 Implementation of the repository pattern for Milvus, the vector database.
 """
+import asyncio
+import os
+
+import httpx
 import pymilvus
 import requests
 from loguru import logger
 from pymilvus import (Collection, CollectionSchema, DataType, FieldSchema,
                       connections, utility)
+from pymilvus.orm.connections import Connections
+
+from transit import iconnection, irepository
 
 # 19530 is for gRPC, which is the default when you connect
 # to a Milvus server with Milvus SDKs
@@ -13,36 +20,184 @@ from pymilvus import (Collection, CollectionSchema, DataType, FieldSchema,
 # to a Milvus server with an HTTP client
 ALLOWED_PORTS = set([19530, 9091])
 
-def milvus_server_is_running(url='http://localhost:9091/api/v1/health'):
-    """
-    Tests if the Milvus server is running.
-    """
-    logger.info(f'Testing if Milvus server is running at {url}...')
-    response = requests.get(url)
-    logger.info(f'Received response status code: {response.status_code}')
-    return response.status_code == 200
 
 
 
-def connect_to_milvus_server(host, port, alias='default'):
-    """
-    Connect to a Milvus server via the gRPC protocol.
-    """
-    if milvus_server_is_running():
-        logger.info(f'Connecting to Milvus server at {host}:{port}...')
-        milvus_connection = connections.connect(
-            alias=alias,
-            host=host,
-            port=port,
-        )
-    return milvus_connection
+
+class FakeMilvusConnection(iconnection.IConnection):
+    def __init__(self, alias=None, host=None, port=None):
+        pass
+
+    def connect(self):
+        logger.info('Connected to FakeMilvusConnection')
+
+    def disconnect(self):
+        logger.info('Disconnected from FakeMilvusConnection')
+
+    @classmethod
+    def local_connection(cls, alias, host, port):
+        return cls(alias=alias, host=host, port=port)
+
+class MilvusConnection(iconnection.IConnection):
+    def __init__(self, alias=None, host=None, port=None):
+        pass
+
+    def connect(self):
+        print('Connected to FakeMilvusConnection')
+
+    def disconnect(self):
+        print('Disconnected from FakeMilvusConnection')
+
+    @classmethod
+    def local_connection(cls, alias, host, port):
+        return cls(alias=alias, host=host, port=port)
 
 
-def create_collection(alias):
-    fields = [
-        FieldSchema(name='pk', dtype=DataType.INT64, is_primary=True, auto_id=False),
-        FieldSchema(name='random', dtype=DataType.DOUBLE),
-        FieldSchema(name='embeddings', dtype=DataType.FLOAT_VECTOR, dim=8)
-    ]
-    schema = CollectionSchema(fields, 'hello_milvus is the simplest demo to introduce the APIs')
-    hello_milvus = Collection(alias, schema)
+
+class FakeMilvusCollection(iconnection.IConnection):
+    def connect(self):
+        logger.info('Connected to FakeMilvusCollection')
+
+    def disconnect(self):
+        logger.info('Disconnected from FakeMilvusCollection')
+
+
+
+
+class MilvusRepository(irepository.IRepository):
+    def __init__(self, alias=None, uri=None, user=None, password=None, collection_name=None, openai_api_key=None):
+        self.alias = alias or 'default'
+        self.uri = uri or os.environ.get('MILVUS_DB_URI')
+        self.user = user or os.environ.get('MILVUS_DB_USER')
+        self.password = password or os.environ.get('MILVUS_DB_PASSWORD')
+        self.collection_name = collection_name or os.environ.get('MILVUS_COLLECTION_NAME')
+        self.openai_api_key = openai_api_key or os.environ.get('OPENAI_API_KEY')
+
+    def __enter__(self):
+        pass
+        # if 'localhost' in self.uri:
+        #     connections.connect("default", host="localhost", port="19530")
+        # else:
+        #     connections.connect(alias=self.alias, uri=self.uri, user=self.user, password=self.password, secure=True)
+        # openai.api_key = self.openai_api_key
+
+        # self.collection = Collection(name=self.collection_name)
+        # self.collection.load()
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+        # connections.disconnect(self.alias)
+        # print("closing milvus connection")
+
+    def get(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def upsert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    # async def create_embedding(self, text):
+    #     async with httpx.AsyncClient() as client:
+    #         url = f'https://api.openai.com/v1/engines/text-embedding-ada-002/embed'
+    #         headers = {
+    #             'Authorization': f'Bearer {self.openai_api_key}',
+    #             'Content-Type': 'application/json'
+    #         }
+    #         data = {'input': text}
+    #         response = await client.post(url, json=data, headers=headers)
+
+    #     return response.json()['data'][0]['embedding']
+
+    # async def insert_data(self, text_chunks):
+    #     ids = []
+    #     embeddings_to_insert = []
+    #     bill_ids = []
+
+    #     tasks = [self.create_embedding(text) for text in text_chunks]
+    #     embeddings = await asyncio.gather(*tasks)
+
+    #     for idx, embedding in enumerate(embeddings):
+    #         ids.append(f'test_{idx}')
+    #         embeddings_to_insert.append(embedding)
+    #         bill_ids.append('test_bill_id')
+
+    #     self.collection.insert(data=[
+    #         ids,
+    #         embeddings_to_insert,
+    #         bill_ids,
+    #     ])
+
+    #     return ids
+
+    # async def search(self, query_text, top_k=10):
+    #     query_embedding = await self.create_embedding(query_text)
+
+    #     search_params = {'metric_type': 'L2', 'params': {'nprobe': 10}}
+    #     results = self.collection.search(
+    #         data=[query_embedding],
+    #         anns_field='embedding',
+    #         param=search_params,
+    #         limit=top_k
+    #     )
+
+    #     return results
+
+    # def delete_data(self, ids):
+    #     for inserted_id in ids:
+    #         self.collection.delete(expr=f"id in ['{inserted_id}']")
+
+    @staticmethod
+    def local_milvus_server_is_running(url='http://localhost:9091/api/v1/health'):
+        """
+        Tests if a local Milvus server is running.
+        """
+        logger.info(f'Testing if a local Milvus server is running at {url}...')
+        response = requests.get(url)
+        logger.info(f'Received response status code: {response.status_code}')
+        return response.status_code == 200
+
+
+    @staticmethod
+    def connect_to_milvus_server(host, port, alias='default'):
+        """
+        Connect to a Milvus server via the gRPC protocol.
+        """
+        if MilvusRepository.local_milvus_server_is_running():
+            logger.info(f'Connecting to Milvus server at {host}:{port}...')
+            milvus_connection = connections.connect(
+                alias=alias,
+                host=host,
+                port=port,
+            )
+        return milvus_connection
+
+    @staticmethod
+    def create_collection(alias):
+        fields = [
+            FieldSchema(name='pk', dtype=DataType.INT64, is_primary=True, auto_id=False),
+            FieldSchema(name='random', dtype=DataType.DOUBLE),
+            FieldSchema(name='embeddings', dtype=DataType.FLOAT_VECTOR, dim=8)
+        ]
+        schema = CollectionSchema(fields, 'hello_milvus is the simplest demo to introduce the APIs')
+        hello_milvus = Collection(alias, schema)
+
+    @staticmethod
+    def get_connections():
+        """
+        This returns a handle to the pymilvus Singleton Connections object.
+        """
+        return Connections()
+
+    @staticmethod
+    def assert_connection_exists(alias):
+        assert MilvusRepository.get_connections().has_connection(alias=alias), f'Connection {alias} does not exist.'
+
+def milvus_factory():
+    pass
